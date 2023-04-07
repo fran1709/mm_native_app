74e643fb18bda33714fd1c2597b827a26c3d9004
Josué Quesada <josuequesadaxd@gmail.com>
2023-04-06 19:37:27 -0600

    updated search
    not fully functional

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,82 @@
+import { Picker } from "@react-native-picker/picker";
 import React from "react";
-import { Button, View, Text, StyleSheet, TextInput} from "react-native";
+import { Button, View, Text, StyleSheet, TextInput } from "react-native";
 
-const Header = ({navigation}) => {
-    const [text, onChangeText] = React.useState('Buscar Comida...');
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Meally Meaty App</Text>
-            <View>
-               <TextInput
-                    editable
-                    maxLength={40}
-                    onChangeText={onChangeText}
-                    placeholder={'Buscar Comida...'}
-                    style={styles.inputText}
-                /> 
-            </View>
-        </View>
-    )
-}
+const Header = ({ navigation, setMeals}) => {
+
+  const [text, onChangeText] = React.useState("Buscar Comida...");
+  const [selectedSearch, setSelectedSearch] = React.useState('');
+
+  const getMealsApi = async () => {
+    var response;
+    switch(selectedSearch){
+        case "name":
+            response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${text}`);
+        case "ingredients":
+            response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${text}`);
+        case "country":
+            response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${text}`);
+    }
+    let json = await response.json();
+    console.log(json);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Meally Meaty App</Text>
+      <View style={styles.searchContainer}>
+        <TextInput
+          editable
+          maxLength={30}
+          onChangeText={onChangeText}
+          placeholder={"Search recipe..."}
+          style={styles.inputText}
+        />
+        <Picker
+          style={styles.picker}
+          selectedValue={selectedSearch}
+          onValueChange={(itemValue) => setSelectedSearch(itemValue)}
+          prompt="Search by:"
+        >
+          <Picker.Item label="Name" value="name" />
+          <Picker.Item label="Ingredient" value="ingredient" />
+          <Picker.Item label="Country" value="country" />
+        </Picker>
+      </View>
+      <Button title="Search" onPress={getMealsApi}>
+
+      </Button>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection:'column',
-        justifyContent: "center",
-        paddingBottom: 5,
-        paddingTop: 5,
-    },
-    inputText:{
-        height: 40,
-        margin: 12,
-        borderWidth: 0.5,
-        padding: 10,
-    },
-    title:{
-        fontWeight: "bold",
-        fontSize: 20,
-        marginLeft:10,
-        padding:10
-    },
+  container: {
+    flexDirection: "column",
+    justifyContent: "center",
+    paddingBottom: 5,
+    paddingTop: 5,
+  },
+  searchContainer: {
+    flexDirection: "row",
+  },
+  inputText: {
+    height: 40,
+    margin: 12,
+    borderWidth: 0.5,
+    padding: 10,
+    width: 170,
+  },
+  picker: {
+    flex: 1,
+    maxWidth: 175,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+    marginLeft: 10,
+    padding: 10,
+  },
 });
 
-export default Header
+export default Header;
